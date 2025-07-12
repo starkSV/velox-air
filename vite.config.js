d63be6873368d00e280057c72d4e914f5522676d
Shekhar Vaidya <shekhar.vaidya2@hotmail.com>
2025-07-12 18:05:02 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/velox-air/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/velox-air/' : '/',
   plugins: [react()],
-})
+}));
